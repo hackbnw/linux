062c20adc67a9361bc8f40c72ebb4c023b0a01eb
hackbnw <yurii.pavlovskyi@gmail.com>
2019-04-09 20:18:55 +0200

    platform/x86: asus-wmi: Improve DSTS WMI method ID detection
    The DSTS method detection mistakenly selects DCTS instead of DSTS if
    nothing is returned when the method ID is not defined in WMNB. As a result,
    the control of keyboard backlight is not functional for TUF Gaming series
    laptops. Implement another detection method instead.
    
    There is evidence that DCTS is handled by ACPI WMI devices that have _UID
    ASUSWMI, whereas none of the devices without ASUSWMI respond to DCTS and
    DSTS is used instead [1]. To check the _UID a new method is added to wmi.h
    / wmi.c. It returns _UID of the ACPI WMI device that declares WMI object
    with given GUID.
    
    Generally, it is possible that multiple PNP0C14 ACPI devices are present in
    the system as mentioned in the commit message of commit bff431e49ff5
    ("ACPI: WMI: Add ACPI-WMI mapping driver").
    
    Therefore the _UID is checked for given GUID that maps to a specific ACPI
    device, to which it is also mapped by other methods of wmi module.
    
    DSDT examples:
    
    FX505GM:
    Method (WMNB, 3, Serialized)
    { ...
        If ((Local0 == 0x53545344))
        {
            ...
            Return (Zero)
        }
        ...
        // No return
    }
    
    K54C:
    Method (WMNB, 3, Serialized)
    { ...
        If ((Local0 == 0x53545344))
        {
            ...
            Return (0x02)
        }
        ...
        Return (0xFFFFFFFE)
    }
    
    [1] https://lkml.org/lkml/2019/4/11/322
    
    Signed-off-by: Yurii Pavlovskyi <yurii.pavlovskyi@gmail.com>
    Suggested-by: Daniel Drake <drake@endlessm.com>

--- drivers/platform/x86/asus-wmi.c
+++ drivers/platform/x86/asus-wmi.c
@@ -80,6 +80,8 @@ MODULE_LICENSE("GPL");
 #define USB_INTEL_XUSB2PR		0xD0
 #define PCI_DEVICE_ID_INTEL_LYNXPOINT_LP_XHCI	0x9c31
 
+#define ASUS_ACPI_UID_ASUSWMI		"ASUSWMI"
+
 static const char * const ashs_ids[] = { "ATK4001", "ATK4002", NULL };
 
 static bool ashs_present(void)
@@ -1847,6 +1849,7 @@ static int asus_wmi_sysfs_init(struct platform_device *device)
 static int asus_wmi_platform_init(struct asus_wmi *asus)
 {
 	int rv;
+	char *wmi_uid;
 
 	/* INIT enable hotkeys on some models */
 	if (!asus_wmi_evaluate_method(ASUS_WMI_METHODID_INIT, 0, 0, &rv))
@@ -1875,11 +1878,24 @@ static int asus_wmi_platform_init(struct asus_wmi *asus)
 	 * Note, on most Eeepc, there is no way to check if a method exist
 	 * or note, while on notebooks, they returns 0xFFFFFFFE on failure,
 	 * but once again, SPEC may probably be used for that kind of things.
+	 *
+	 * Additionally at least TUF Gaming series laptops return nothing for
+	 * unknown methods, so the detection in this way is not possible.
+	 *
+	 * There is strong indication that only ACPI WMI devices that have _UID
+	 * equal to "ASUSWMI" use DCTS whereas those with "ATK" use DSTS.
 	 */
-	if (!asus_wmi_evaluate_method(ASUS_WMI_METHODID_DSTS, 0, 0, NULL))
+	wmi_uid = wmi_get_acpi_device_uid(ASUS_WMI_MGMT_GUID);
+	if (!wmi_uid)
+		return -ENODEV;
+
+	if (!strcmp(wmi_uid, ASUS_ACPI_UID_ASUSWMI)) {
+		pr_info("Detected ASUSWMI, use DCTS\n");
 		asus->dsts_id = ASUS_WMI_METHODID_DSTS;
-	else
+	} else {
+		pr_info("Detected %s, not ASUSWMI, use DSTS\n", wmi_uid);
 		asus->dsts_id = ASUS_WMI_METHODID_DSTS2;
+	}
 
 	/* CWAP allow to define the behavior of the Fn+F2 key,
 	 * this method doesn't seems to be present on Eee PCs */
